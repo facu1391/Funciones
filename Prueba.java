cef666471fb1aa36145c9a79c2b241ef61051cba
Facundo Nuñez <105162692+facu1391@users.noreply.github.com>
2023-02-15 17:14:13 -0300

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,44 @@
+public class Prueba {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.nombre = "juan";
+        cliente.edad = 31;
+        cliente.telefono = 452145996;
+        cliente.credito = "Select";
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.nombre = "Pedro";
+        trabajador.edad = 40;
+        trabajador.telefono = 4431351;
+        trabajador.salario = "$15000";
+
+        System.out.println(cliente.nombre);
+        System.out.println(cliente.edad);
+        System.out.println(cliente.edad);
+        System.out.println(cliente.telefono);
+        System.out.println(cliente.credito);
+
+        System.out.println(trabajador.nombre);
+        System.out.println(trabajador.edad);
+        System.out.println(trabajador.telefono);
+        System.out.println(trabajador.salario);
+    }
+}
+
+
+class Persona {
+    String nombre;
+    int edad;
+    int telefono;
+}
+
+class Cliente extends Persona {
+    String credito;
+}
+
+class Trabajador extends Persona {
+    String salario;
+}
